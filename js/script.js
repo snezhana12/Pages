5588786e288633a0799a5b2e6f8b3ff8303378a1
snezhana12 <107568693+snezhana12@users.noreply.github.com>
2022-09-25 19:01:04 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+new Swiper('.swiper', {
+    duration: 1,
+    spaceBetween: 30,
+    speed: 600,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+
+
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true
+    }
+
+
+});
